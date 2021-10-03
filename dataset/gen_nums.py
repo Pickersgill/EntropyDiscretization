f2df26148c5ca1a8bfd462fe1bc2ce094a6d9b2e
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2021-10-03 19:17:18 +0100

    Added labels seporate to values in source data.

--- dataset/gen_nums.py
+++ dataset/gen_nums.py
@@ -4,14 +4,17 @@ import sys
 # SMALL UNIFORMALLY RANDOM DATASET
 def make_data1(f):
     for i in range(200):
-        r = random.randint(1, 10)
-        f.write(str(r) + "\n")
+        attr1 = str(random.randint(1, 1000))
+        label = str(random.randint(1, 10))
+        f.write(", ".join([attr1, label]) + "\n")
 
 # LARGER MONOTONAL DATASET
 def make_data2(f):
     for i in range(200):
         for j in range(random.randint(1, 10)):
-            f.write(str(i) + "\n")
+            attr1 = str(random.randint(1, 1000))
+            label = str(i)
+            f.write(", ".join([attr1, label]) + "\n")
 
 f = open("numbers", "w")
 
